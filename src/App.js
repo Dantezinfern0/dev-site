c1fd2607b7010a5ba100c6c8db7894ddcb78801a
Dante Harasz <danteharasz@gmail.com>
2020-04-10 14:55:38 -0400

    added images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
 const App = () => {
-  return <HelloWorld />
+  return <div>
+    hello world
+  </div>
 }
 
 export default App
