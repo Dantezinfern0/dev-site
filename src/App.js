2a6600918ea663d1cc7aa20175a2a847ea9c1309
Dante Harasz <danteharasz@gmail.com>
2020-04-29 17:52:03 -0400

    weather page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Dante from './pages/Dante'
 import Diana from './pages/Diana'
+import WeatherWidget from './components/WeatherWidget'
 import Rotator from './components/Rotator'
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
                         <Route exact path="/dante" component={Dante}></Route>
                         <Route exact path="/diana" component={Diana}></Route>
                         <Route exact path="/projects" component={Rotator}></Route>
+                        <Route exact path="/weather" component={WeatherWidget}></Route>
                     </Switch>
                 </BrowserRouter>
                 <Footer />
