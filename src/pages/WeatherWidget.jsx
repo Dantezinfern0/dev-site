e222dd74125855db613bcd18897c7adeace3f266
Dante Harasz <danteharasz@gmail.com>
2020-11-21 14:09:38 -0500

    converted widget to RCC

--- src/pages/WeatherWidget.jsx
+++ src/pages/WeatherWidget.jsx
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react'
+// import React, {useState, useEffect} from 'react'
+import React, { Component } from 'react'
 import Moment from 'react-moment'
 import '../styles/widget.css'
 
@@ -20,111 +21,123 @@ const indicate = {
   clear: "https://cdn.pixabay.com/photo/2017/11/04/08/14/tree-2916763_1280.jpg"
 }
 
-const WeatherWidget = () => {
-  // const  [errors, setErrors] =  useState('')
-  const  [weather,setWeather] = useState({})
 
-  useEffect(() => {
+  export default class WeatherWidget extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        currentTemp: "",
+        currentDescription: "",
+        currentMessage: "",
+        currentWindSpeed: "",
+        currentWindDir: "",
+        currentWindDeg: "",
+        dailyMax: "",
+        dailyMin: "",
+        sunrise: "",
+        sunset: "",
+        time: ""
+      }
+    }
+    convert(k) {
+      k += -273.15
+      let f = k * 9/5 
+      f += 32
+      return Math.round(f) + '°F'
+    }
+    toTitleCase(str) {
+      return str.replace(
+          /\w\S*/g,
+          function(txt) {
+              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+          }
+      );
+  }
+    direction() {
+      let deg = this.state.currentWindDeg
+      if (338 < deg < 24) {
+        return 'North'
+      } else if (295 < deg < 337) {
+        return 'NorthWest'
+      } else if (248 < deg < 294) {
+        return 'West'
+      } else if (203 < deg < 247) {
+        return 'SouthWest'
+      } else if (157 < deg < 202) {
+        return 'South'
+      } else if (112 < deg < 156) {
+        return 'SouthEast'
+      } else if (69 < deg < 111) {
+        return 'East'
+      } else if (25 < deg < 68) {
+        return 'NorthEast'
+      } else {
+          return 'Bonkers'
+      }
+    }
+    photo() {
+      let itIs = this.state.currentDescription
+      if ( itIs === 'Rain') {
+        return indicate.rain
+      } else if (itIs === 'Thunderstorm') {
+        return indicate.thunderstorm
+      } else if (itIs === 'Drizzle') {
+        return indicate.drizzle
+      } else if (itIs === 'Clouds') {
+        return indicate.cloudy
+      } else if (itIs === 'Snow') {
+        return  indicate.snow
+      } else {
+        return indicate.clear
+      }
+    }
+    componentDidMount() {
     fetch(`${url}?lat=${pitts.lat}&lon=${pitts.lon}&exclude=${part}&appid=${key}`)
     .then(obj => {
       return obj.json()
     })
     .then(resp => {
-        setWeather({
-          currentTemp: convert(resp.current.temp),
+        this.setState({
+          currentTemp: this.convert(resp.current.temp),
           currentDescription: resp.current.weather[0].main,
-          currentMessage: toTitleCase(resp.current.weather[0].description),
+          currentMessage: this.toTitleCase(resp.current.weather[0].description),
           currentWindSpeed: resp.current.wind_speed + 'mph',
           currentWindDir: resp.current.wind_deg + '°',
           currentWindDeg: resp.current.wind_deg,
-          dailyMax: convert(resp.daily[0].temp.max),
-          dailyMin: convert(resp.daily[0].temp.min),
+          dailyMax: this.convert(resp.daily[0].temp.max),
+          dailyMin: this.convert(resp.daily[0].temp.min),
           sunrise: resp.current.sunrise,
           sunset: resp.current.sunset,
           time: resp.current.dt
         })
         // console.log(resp)
-      })},[])
+      })}
 
-  const convert = (k) => {
-    k += -273.15
-    let f = k * 9/5 
-    f += 32
-    return Math.round(f) + '°F'
-  }
-  const toTitleCase = (str) => {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
-}
-  const direction = () => {
-    let deg = weather.currentWindDeg
-    if (338 < deg < 24) {
-      return 'North'
-    } else if (295 < deg < 337) {
-      return 'NorthWest'
-    } else if (248 < deg < 294) {
-      return 'West'
-    } else if (203 < deg < 247) {
-      return 'SouthWest'
-    } else if (157 < deg < 202) {
-      return 'South'
-    } else if (112 < deg < 156) {
-      return 'SouthEast'
-    } else if (69 < deg < 111) {
-      return 'East'
-    } else if (25 < deg < 68) {
-      return 'NorthEast'
-    } else {
-        return 'Bonkers'
-    }
-  }
-  const photo = () => {
-    let itIs = weather.currentDescription
-    if ( itIs === 'Rain') {
-      return indicate.rain
-    } else if (itIs === 'Thunderstorm') {
-      return indicate.thunderstorm
-    } else if (itIs === 'Drizzle') {
-      return indicate.drizzle
-    } else if (itIs === 'Clouds') {
-      return indicate.cloudy
-    } else if (itIs === 'Snow') {
-      return  indicate.snow
-    } else {
-      return indicate.clear
-    }
-  }
-
+render() {
   return (
     <div>
-   <div className="center-flex" style={{backgroundImage: `url(${photo()})`}}>
+   <div className="center-flex" style={{backgroundImage: `url(${this.photo()})`}}>
     <section>
     <h1>Pittsburgh, PA</h1>
     <h3>
-      <Moment unix format="MM/DD/YYYY">{weather.time}</Moment>
+      <Moment unix format="MM/DD/YYYY">{this.state.time}</Moment>
     </h3>
-    <h3>{weather.currentMessage}</h3>
-    <h1>{weather.currentTemp}</h1>
-    <h4>High: {weather.dailyMax}</h4>
-    <h4>Low: {weather.dailyMin}</h4>
+    <h3>{this.state.currentMessage}</h3>
+    <h1>{this.state.currentTemp}</h1>
+    <h4>High: {this.state.dailyMax}</h4>
+    <h4>Low: {this.state.dailyMin}</h4>
     <h4>{'Sunrise: '}
-    <Moment unix format="HH:MM">{weather.sunrise}</Moment></h4>
+    <Moment unix format="HH:MM">{this.state.sunrise}</Moment></h4>
     <h4>{'Sunset: '}
-    <Moment unix format="HH:MM">{weather.sunset}</Moment></h4>
+    <Moment unix format="HH:MM">{this.state.sunset}</Moment></h4>
     <h6>
-      Wind speed: {weather.currentWindSpeed}
+      Wind speed: {this.state.currentWindSpeed}
       {' '}
-      {direction()} @ {weather.currentWindDir}
+      {this.direction()} @ {this.state.currentWindDir}
     </h6>
-    <img id="indicator" alt="#" src={photo()} />
+    <img id="indicator" alt="#" src={this.photo()} />
   </section>
   <p>Widget by DankDevTeam</p>
 </div>
 </div>)
-}
-
-export default WeatherWidget
+}}
