8962855fb624b010004af65b83aaf6f9c35b8a68
Dante Harasz <danteharasz@gmail.com>
2021-03-11 16:22:06 -0500

    Randomized Affiliate list for fairness

--- src/components/Affiliates.jsx
+++ src/components/Affiliates.jsx
@@ -8,13 +8,13 @@ import {
 import FriendLink from './FriendLink'
 import Friends from '../data/Friends'
 
-
 const Affiliates = () => {
+    let shuffledFriends = Friends.map((a) => ({sort: Math.random(), value: a})).sort((a, b) => a.sort - b.sort).map((a) => a.value)
     return <Jumbotron>
         <Container>
             <h1>Affiliate Links</h1>
             <Col>
-                {Friends.map((f, i) => {return <FriendLink key={i} url={f.url} author={f.author} title={f.title} />})}
+                {shuffledFriends.map((f, i) => {return <FriendLink key={i} url={f.url} author={f.author} title={f.title} />})}
             </Col>
         </Container>
     </Jumbotron>
