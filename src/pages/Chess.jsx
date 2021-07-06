d4a5938a6acfe368034fc73a942837c7ceb9b914
Dante Harasz <danteharasz@gmail.com>
2021-07-06 12:00:01 -0400

    spinkit for iframes

--- src/pages/Chess.jsx
+++ src/pages/Chess.jsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, {useState} from 'react'
+import Spinner from 'react-spinkit';
 
 const Chess = () => {
+    const [loadingPuzzle, setLoadingPuzzle] = useState(true)
+    const [loadingTv, setLoadingTv] = useState(true)
     return (
         <div className="chess-container">
             <h1>Daily Chess Puzzle</h1>
             <div>
+            {loadingPuzzle ? (<Spinner
+                className="loading text-center"
+                name="three-bounce"
+                color="white"
+                fadeIn="none"
+            />) : null}
             <iframe title="chess puzzle"
                 src="https://lichess.org/training/frame?theme=newspaper&bg=dark" 
                 width="400px" 
                 height="444px" 
                 allowtransparency="true" 
                 frameBorder="0"
-                crossOrigin="true"></iframe>
+                crossOrigin="true"
+                onLoad={() => setLoadingPuzzle(false)}
+                ></iframe>
             </div>
             <h1>Chess TV</h1>
             <div>
+            {loadingTv ? (<Spinner
+                className="loading text-center"
+                name="three-bounce"
+                color="white"
+                fadeIn="none"
+            />) : null}
             <iframe title="ChessTV from Lichess.org" 
                 src="https://lichess.org/tv/frame?theme=brown&bg=dark" 
                 width="400px"
                 height="444px"
                 allowtransparency="true" 
                 frameBorder="0" 
-                crossOrigin="true"></iframe>
+                crossOrigin="true"
+                onLoad={() => setLoadingTv(false)}
+                ></iframe>
             </div>
         </div>
     )
