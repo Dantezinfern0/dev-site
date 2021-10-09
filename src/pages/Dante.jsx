8af6846ae4f097fbd34bb437acf5db7f7735e57d
Dante Harasz <danteharasz@gmail.com>
2021-10-09 13:57:18 -0400

    Credentials added

--- src/pages/Dante.jsx
+++ src/pages/Dante.jsx
@@ -6,16 +6,16 @@ import {
     Jumbotron
 } from 'reactstrap';
 import logos from '../data/Logo';
-import CV from '../components/CV'
-import LinkedIn from '../components/LinkedIn'
 import Github from '../components/Github'
 import EmailMe from '../components/EmailMe'
 import DantePortrait from '../images/danteportrait.jpg'
 import Shopify from '../images/shopify-partner.jpg'
+import Certificate from '../images/certification.jpg'
+import Degree from '../images/diploma.jpg'
 
 
 const description = "In 2019 Dante attended code school at Suncoast Developer's guild in St Petersburg, FL.  Since then he has been freelancing and contributing to open source project Lichess.org"
-const linkedIn = "https://www.linkedin.com/in/dante-harasz"
+
 
 
 const Dante = () => {
@@ -30,15 +30,17 @@ const Dante = () => {
                                         {description}
                                     </span>
                                     <br />
-                                    <LinkedIn link={linkedIn} />
                                     <Github  />
-                                    <CV />
                                     <EmailMe />
-                                <img src={Shopify} alt="Shopify Partner Logo"/>
+                                    <Row>
+                                        <img src={Certificate} alt="Web Development Certification" />
+                                        <img src={Degree} alt="Diploma for Bachelor of Science in Psychology" />
+                                    </Row>
                                 </p>
                             </Col>
                             <Col>
                                 <img id="aDante" src={DantePortrait} alt="cartoon avatar of dante" />
+                                <img src={Shopify} alt="Shopify Partner Logo"/>
                             </Col>
                         </Row>
                                 {logos.map((logo, i) => {return <img className="icon-img" key={i} src={logo.src} alt={logo.altText} />})}
